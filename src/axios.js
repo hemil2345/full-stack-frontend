528c3d47fa35e7317925a0f9a1cf30a0cccc2dde
hemil2345 <hemilsuhagiya05@gmail.com>
2025-05-26 02:50:32 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://273b-3-89-143-79.ngrok-free.app/api',
 });
 
 api.interceptors.request.use((config) => {
