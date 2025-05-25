526726b0e3e7b0c805e81f0c7cf72728eeff69e9
hemil2345 <hemilsuhagiya05@gmail.com>
2025-05-26 01:36:46 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'http://3.89.143.79/api',
 });
 
 api.interceptors.request.use((config) => {
